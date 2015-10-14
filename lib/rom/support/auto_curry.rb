42a9f1436e6368c28b0e0d761d73c7f1652aa6e5
Piotr Solnica <piotr.solnica@gmail.com>
2015-10-14 17:24:43 +0200

    Add auto_curry_guard

--- lib/rom/support/auto_curry.rb
+++ lib/rom/support/auto_curry.rb
@@ -1,27 +1,43 @@
+require 'byebug'
+
 module ROM
   module AutoCurry
     def self.extended(klass)
-      busy = false
-
       klass.define_singleton_method(:method_added) do |name|
-        return if busy
-        busy = true
-        auto_curry(name)
-        busy = false
+        return if auto_curry_busy?
+        auto_curry_guard { auto_curry(name) }
         super(name)
       end
     end
 
-    def auto_curry(name)
+    def auto_curry_guard
+      @__auto_curry_busy__ = true
+      yield
+    ensure
+      @__auto_curry_busy__ = false
+    end
+
+    def auto_curry_busy?
+      @__auto_curry_busy__ ||= false
+    end
+
+    def auto_curry(name, &block)
       curried = self.curried
       meth = instance_method(name)
       arity = meth.arity
 
       define_method(name) do |*args|
-        if arity < 0 || arity == args.size
-          meth.bind(self).(*args)
+        response =
+          if arity < 0 || arity == args.size
+            meth.bind(self).(*args)
+          else
+            curried.new(self, name: name, curry_args: args, arity: arity)
+          end
+
+        if block
+          response.instance_exec(&block)
         else
-          curried.new(self, name: name, curry_args: args, arity: arity)
+          response
         end
       end
     end
---
+++ spec/unit/rom/support/auto_curry_spec.rb
@@ -0,0 +1,53 @@
+require 'rom/support/auto_curry'
+
+RSpec.describe ROM::AutoCurry do
+  subject(:object) { klass.new }
+
+  let(:klass) do
+    Class.new do
+      extend ROM::AutoCurry
+
+      def self.curried(*)
+        self
+      end
+
+      def initialize(*)
+      end
+
+      def arity_0
+        0
+      end
+
+      def arity_1(x)
+        x
+      end
+
+      def arity_2(x, y)
+        [x,y]
+      end
+
+      def arity_many(*args)
+        args
+      end
+    end
+  end
+
+  it 'auto-curries method with arity == 0' do
+    expect(object.arity_0).to be(0)
+  end
+
+  it 'auto-curries method with arity == 1' do
+    expect(object.arity_1).to be_instance_of(klass)
+    expect(object.arity_1(1)).to be(1)
+  end
+
+  it 'auto-curries method with arity > 0' do
+    expect(object.arity_2).to be_instance_of(klass)
+    expect(object.arity_2(1, 2)).to eql([1, 2])
+  end
+
+  it 'auto-curries method with arity < 0' do
+    expect(object.arity_many).to eql([])
+    expect(object.arity_many(1, 2)).to eql([1, 2])
+  end
+end
