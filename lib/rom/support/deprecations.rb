0ef22fe53bcb092865e087289f33cba67e41a330
Piotr Solnica <piotr.solnica@gmail.com>
2016-07-13 15:06:21 +0200

    Enhance Deprecations

--- lib/rom/support/deprecations.rb
+++ lib/rom/support/deprecations.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 module ROM
   module Deprecations
     # @api private
@@ -25,12 +27,38 @@ module ROM
       end
     end
 
+    def self.warn(msg)
+      logger.warn(msg.gsub(/^\s+/, ''))
+    end
+
     def self.announce(name, msg)
-      warn <<-MSG.gsub(/^\s+/, '')
-        #{name} deprecated and will be removed in 1.0.0.
+      warn <<-MSG
+        #{name} is deprecated and will be removed in 1.0.0.
+        #{message(msg)}
+      MSG
+    end
+
+    def self.message(msg)
+      <<-MSG
         #{msg}
         #{caller.detect { |l| !l.include?('lib/rom')}}
       MSG
+    end
+
+    def self.logger(output = nil)
+      if defined?(@logger)
+        @logger
+      else
+        set_logger!(output)
+      end
+    end
+
+    def self.set_logger!(output = nil)
+      @logger = Logger.new(output || $stdout)
+      @logger.formatter = proc { |severity, datetime, progname, msg|
+        "[rom] #{msg.dump}\n"
+      }
+      @logger
     end
   end
 end
---
+++ spec/unit/rom/support/deprecations_spec.rb
@@ -0,0 +1,32 @@
+require 'rom/support/deprecations'
+require 'tempfile'
+
+RSpec.describe ROM::Deprecations do
+  let(:log_file) do
+    Tempfile.new
+  end
+
+  before do
+    ROM::Deprecations.set_logger!(log_file)
+  end
+
+  let(:output) do
+    log_file.close
+    log_file.open.read
+  end
+
+  describe '.warn' do
+    it 'logs a warning message' do
+      ROM::Deprecations.warn('hello world')
+      expect(output).to include('[rom] "hello world"')
+    end
+  end
+
+  describe '.announce' do
+    it 'warns about a deprecated method' do
+      ROM::Deprecations.announce(:foo, 'hello world')
+      expect(output).to include('[rom] "foo is deprecated and will be removed')
+      expect(output).to include('hello world')
+    end
+  end
+end
